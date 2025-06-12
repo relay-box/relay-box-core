82e72c640cde12e05b96c674e16ee19a66c7b294
CRooi <contact@crooi.io>
2025-06-12 13:22:51 +0800

    feat: add orm support

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import { sql } from 'drizzle-orm'
+import { int, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
+
+export const users = sqliteTable(
+    'users',
+    {
+        id: int().primaryKey({ autoIncrement: true }),
+        username: text().notNull(),
+        email: text().notNull().unique(),
+        password: text().notNull(),
+        createdAt: text()
+            .notNull()
+            .default(sql`CURRENT_TIMESTAMP`),
+        updatedAt: text()
+            .notNull()
+            .default(sql`CURRENT_TIMESTAMP`)
+    },
+    table => [uniqueIndex('email_idx').on(table.email)]
+)
+
+export type User = typeof users.$inferSelect
+export type InsertUser = typeof users.$inferInsert
